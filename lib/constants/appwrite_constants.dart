5f4292d94261461f32de9f60b210b341ab01ecda
SaurabPoudel <poudelsaurab20@gmail.com>
2023-12-12 11:49:34 +0545

    app bar

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const databaseId = '6577ea428fe4687a92fc';
+  static const projectId = '6575e050ad09978dd6d6';
+  static const endPoint = 'https://appwrite.io/v1';
+}
